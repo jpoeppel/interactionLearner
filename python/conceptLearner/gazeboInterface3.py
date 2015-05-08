fixed transformation error with quaternions (hopefully). however for simulation task, we still need to retransform the old prediction, when that is used as the new worldstate

python/conceptLearner/gazeboInterface3.py
@@ -208,15 +208,16 @@ class GazeboInterface():
         if quat != None:
             q2 = ori
             q1 = quat
-#            q1[:3] *= -1
-#            msg.pose.orientation.x = q1[0]*q2[3]+q1[1]*q2[2]-q1[2]*q2[1]+q1[3]*q2[0]
-#            msg.pose.orientation.y = -q1[0]*q2[2]+q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1]
-#            msg.pose.orientation.z = q1[0]*q2[1]-q1[1]*q2[0]+q1[2]*q2[3]+q1[3]*q2[2]
-#            msg.pose.orientation.w = -q1[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]+q1[3]*q2[3]
-            msg.pose.orientation.x = q1[0]*q2[1]+q1[1]*q2[0]+q1[2]*q2[3]-q1[3]*q2[2]
-            msg.pose.orientation.y = q1[0]*q2[2]-q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1]
-            msg.pose.orientation.z = q1[0]*q2[3]+q1[1]*q2[2]-q1[2]*q2[1]+q1[3]*q2[0]
-            msg.pose.orientation.w = q1[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]-q1[3]*q2[3]
+            newOri = np.zeros(4)
+            newOri[0] = q1[0]*q2[3]+q1[1]*q2[2]-q1[2]*q2[1]+q1[3]*q2[0]
+            newOri[1] = -q1[0]*q2[2]+q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1]
+            newOri[2] = q1[0]*q2[1]-q1[1]*q2[0]+q1[2]*q2[3]+q1[3]*q2[2]
+            newOri[3] = -q1[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]+q1[3]*q2[3]
+            newOri /= np.linalg.norm(newOri)
+            msg.pose.orientation.x = newOri[0]
+            msg.pose.orientation.y = newOri[1]
+            msg.pose.orientation.z = newOri[2]
+            msg.pose.orientation.w = newOri[3]
         else:
             msg.pose.orientation.x = ori[0]
             msg.pose.orientation.y = ori[1]
@@ -255,19 +256,25 @@ class GazeboInterface():
             Protobuf bytearray containing a list of models
         """
         worldState = worldState_pb2.WorldState.FromString(data)
-
-        w = model.WorldState()
+        if self.lastPrediction != None:
+#            print "Parsing worldState with last coordinate system."
+            resultWS = model.WorldState(self.lastPrediction.transM, self.lastPrediction.invTrans, self.lastPrediction.quat)
+            resultWS.parse(worldState)
+        else:
+            resultWS = None
+#        print "parsing new WorldState"
+        newWS = model.WorldState()
         
-        w.parse(worldState)
+        newWS.parse(worldState)
         
         if MODE == FREE_EXPLORATION:
-            self.randomExploration(w)
+            self.randomExploration(newWS, resultWS)
         elif MODE == PUSHTASK:
-            self.pushTask(w)
+            self.pushTask(newWS, resultWS)
         elif MODE == PUSHTASKSIMULATION:
-            self.pushTaskSimulation(w)
+            self.pushTaskSimulation(newWS, resultWS)
         elif MODE == MOVE_TO_TARGET:
-            self.moveToTarget(w)
+            self.moveToTarget(newWS, resultWS)
         else:
             raise AttributeError("Unknown MODE: ", MODE)
 
@@ -337,7 +344,7 @@ class GazeboInterface():
             self.tmpBlockErrorPos += np.linalg.norm(blockPrediction["spos"]-blockInt["spos"]) 
             self.tmpBlockErrorOri +=  np.linalg.norm(blockPrediction["sori"]-blockInt["sori"])
 
-    def pushTask(self, worldState):
+    def pushTask(self, worldState, resultState=None):
         """
         Task to push straight against a block for NUM_TRAIN_RUNS times to train the model,
         which is then tested for NUM_TEST_RUNS afterwards to compute the prediction error.
@@ -401,7 +408,7 @@ class GazeboInterface():
             self.blockErrorPos = 0.0
             self.blockErrorOri = 0.0
             
-    def pushTaskSimulation(self, worldState):
+    def pushTaskSimulation(self, worldState, resultState=None):
         self.stepCounter += 1
         print "num cases: " + str(len(self.worldModel.cases))
         print "num abstract cases: " + str(len(self.worldModel.abstractCases))
@@ -421,7 +428,7 @@ class GazeboInterface():
         if self.trainRun < NUM_TRAIN_RUNS:
             print "Train run #: ", self.trainRun
             if self.runStarted:
-                self.updateModel(worldState)
+                self.updateModel(worldState, resultState)
             else:
                 self.trainRun += 1
                 if self.trainRun == NUM_TRAIN_RUNS:
@@ -432,7 +439,10 @@ class GazeboInterface():
                 self.lastAction = model.Action(cmd = GAZEBOCMDS["MOVE"], direction=np.array([0.0,0.5,0.0]))
                 if self.lastPrediction != None:
                     worldState = self.lastPrediction
+                    #Retransform
+                    print "lastPrediction=worldState: ", worldState.interactionStates
                 self.lastPrediction = self.worldModel.predict(worldState, self.lastAction)
+                print "lastAction: ", self.lastAction
                 self.sendPrediction()
                 self.sendCommand(self.lastAction)
             else:
@@ -446,7 +456,7 @@ class GazeboInterface():
         print "numPredictions: ", self.worldModel.numPredictions
         
             
-    def updateModel(self, worldState):
+    def updateModel(self, worldState, resultState):
         """
         Function to perform the world update and get the next prediction.
         Currently action NOTHING is performed in here.
@@ -456,8 +466,8 @@ class GazeboInterface():
         worldState: mode.WorldState
             The current world state
         """
-        if self.lastPrediction != None:
-            self.worldModel.update(self.lastState, self.lastAction, self.lastPrediction, worldState)
+        if self.lastPrediction != None and resultState != None:
+            self.worldModel.update(self.lastState, self.lastAction, self.lastPrediction, resultState)
         
         self.lastState = worldState
 #        if self.stepCounter == 1:
@@ -469,7 +479,7 @@ class GazeboInterface():
         self.sendPrediction()
         self.sendCommand(self.lastAction)
 
-    def randomExploration(self, worldState):
+    def randomExploration(self, worldState, resultState):
         """
         Task to perform random/free exploration, driven by the model itself.
                 
@@ -480,7 +490,7 @@ class GazeboInterface():
             The current world state
         """
         if self.lastPrediction != None:
-            self.worldModel.update(self.lastState, self.lastAction,self.lastPrediction, worldState)
+            self.worldModel.update(self.lastState, self.lastAction,self.lastPrediction, resultState)
             
         tmp = self.worldModel.getAction(worldState)
         

python/conceptLearner/model4.py
@@ -150,23 +150,30 @@ class ObjectState(State):
                           [2*x*z+2*w*y,2*y*z-2*w*x, 1-2*x*x-2*y*y, pz],[0.0,0.0,0.0,1.0]])
                           
     def transform(self, matrix, q1):
-        print "calling transform for: ", self["name"]
+#        print "calling transform for: ", self["name"]
         tmpPos = np.matrix(np.concatenate((self["pos"],[1])))
 #        print "tmpPos: {}, matrix: {}".format(tmpPos, matrix)
         self["pos"] = np.array((matrix*tmpPos.T)[:3]).flatten()
-        print "original pos: {}, result pos: {}".format(tmpPos, self["pos"])
+#        print "original pos: {}, result pos: {}".format(tmpPos, self["pos"])
         q2 = self["orientation"]
         q1[:3] *= -1
 #        self["orientation"] = np.array([q1[0]*q2[3]+q1[1]*q2[2]-q1[2]*q2[1]+q1[3]*q2[0],
 #                                -q1[0]*q2[2]+q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1],
 #                                q1[0]*q2[1]-q1[1]*q2[0]+q1[2]*q2[3]+q1[3]*q2[2],
 #                                -q1[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]+q1[3]*q2[3]])
-        self["orientation"] = np.array([q1[0]*q2[1]+q1[1]*q2[0]+q1[2]*q2[3]-q1[3]*q2[2],
-                                q1[0]*q2[2]-q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1],
-                                q1[0]*q2[3]+q1[1]*q2[2]-q1[2]*q2[1]+q1[3]*q2[0],
-                                q1[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]-q1[3]*q2[3]])
-        self["orientation"] /= np.linalg.norm(self["orientation"])
-        print "original ori: {}, resulting ori: {}".format(q2, self["orientation"])
+#        self["orientation"] = np.array([q1[0]*q2[1]+q1[1]*q2[0]+q1[2]*q2[3]-q1[3]*q2[2],
+#                                q1[0]*q2[2]-q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1],
+#                                q1[0]*q2[3]+q1[1]*q2[2]-q1[2]*q2[1]+q1[3]*q2[0],
+#                                q1[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]-q1[3]*q2[3]])
+        newOri = np.zeros(4)
+        newOri[0] = q1[0]*q2[3]+q1[1]*q2[2]-q1[2]*q2[1]+q1[3]*q2[0]
+        newOri[1] = -q1[0]*q2[2]+q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1]
+        newOri[2] = q1[0]*q2[1]-q1[1]*q2[0]+q1[2]*q2[3]+q1[3]*q2[2]
+        newOri[3] = -q1[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]+q1[3]*q2[3]
+        newOri /= np.linalg.norm(newOri)
+#        self["orientation"] /= np.linalg.norm(self["orientation"])
+        self["orientation"] = newOri
+#        print "original ori: {}, resulting ori: {}".format(q2, self["orientation"])
         tmplV = np.matrix(np.concatenate((self["linVel"],[0])))
         self["linVel"] = np.array((matrix*tmplV.T)[:3]).flatten()
         tmpaV = np.matrix(np.concatenate((self["angVel"],[0])))
@@ -272,14 +279,14 @@ class Action(State):
         
 class WorldState(object):
     
-    def __init__(self):
+    def __init__(self, transM = None, invTrans = None, quat = None):
         self.objectStates = {}
         self.interactionStates = {}
         self.numIntStates = 0
         self.predictionCases = {}
-        self.transM = None
-        self.invTrans = None
-        self.quat = None
+        self.transM = transM
+        self.invTrans = invTrans
+        self.quat = quat
 
     def addInteractionState(self, intState, usedCase = None):
 #        print "adding interactionState: ", intState["intId"]
@@ -307,13 +314,13 @@ class WorldState(object):
                 tmp["type"] = m.type
                 self.objectStates[m.name] = tmp
                 
-                if m.name == "blockA":
+                if m.name == "blockA" and self.transM == None:
                     self.transM = tmp.getTranformationMatrix()
                     self.invTrans = np.matrix(np.zeros((4,4)))
                     self.invTrans[:3,:3] = self.transM[:3,:3].T
                     self.invTrans[:3,3] = -self.transM[:3,:3].T*self.transM[:3,3]
                     self.invTrans[3,3] = 1.0
-                    self.quat = tmp["orientation"]
+                    self.quat = np.copy(tmp["orientation"])
 #                    self.quat[:3] *= -1
 #                print "BlockA angVel: ", tmp["angVel"]
                 
@@ -345,14 +352,18 @@ class WorldState(object):
         self.parseModels(gzWS.model_v.models)
         self.parseContacts(gzWS.contacts.contact)
         self.parseInteractions(gzWS)
-        
-        print "InteractionStates: ", self.interactionStates.values()
+#        
+#        print "InteractionStates: ", self.interactionStates.values()
 
     def getInteractionState(self, sname):
         for i in self.interactionStates.values():
             if i["sname"] == sname:
                 return i
         return None    
+        
+    def retransform(self):
+        blockState = self.getInteractionState("blockA")
+        
 
 class BaseCase(object):
     
@@ -467,7 +478,7 @@ class AbstractCase(object):
 #            print "resultState intId after: ", resultState["intId"]
             
         else:
-            print "predicting with only one ref"
+#            print "predicting with only one ref"
             for k in self.variables:
                 resultState[k] = state[k] + self.refCases[0].predict(state, action, k)
 #            prediction= self.refCases[0].predict(state,action)
@@ -754,7 +765,7 @@ class AbstractCase(object):
         else:
             intState = worldState.getInteractionState("gripper")
             
-        print "choosing state with name: ", intState["sname"]
+#        print "choosing state with name: ", intState["sname"]
         target = copy.deepcopy(intState)
         for k in self.variables:
             if hasattr(target[k], "__len__"):
@@ -808,11 +819,11 @@ class ModelCBR(object):
                         action = ac.getAction(gripperInt,variables, self.target.weights, dif)
                         prediction = ac.predict(gripperInt, action)
                         score = self.target.score(prediction)#*ac.avgPrediction
-                        print "abstract case: ", ac.variables
-                        print "ac avgPrediction: ", ac.avgPrediction
-                        print "numPredictions: ", ac.numPredictions
-                        print "predicted pos: ", prediction["spos"]
-                        print "score to target: {}, for action: {}".format(score, action)                    
+#                        print "abstract case: ", ac.variables
+#                        print "ac avgPrediction: ", ac.avgPrediction
+#                        print "numPredictions: ", ac.numPredictions
+#                        print "predicted pos: ", prediction["spos"]
+#                        print "score to target: {}, for action: {}".format(score, action)                    
                         if score > bestScore:
                             bestScore = score
                             bestAction = action
@@ -884,8 +895,8 @@ class ModelCBR(object):
             bestCase = sortedList[0][0]
         if isinstance(bestCase, AbstractCase):
 #            print "bestCase #refs: ", len(bestCase.refCases)
-            print "bestCase variables: ", bestCase.variables
-            print "bestCase constants: ", bestCase.constants
+#            print "bestCase variables: ", bestCase.variables
+#            print "bestCase constants: ", bestCase.constants
             if bestCase.variables == []:
                 self.numZeroCase += 1
 #            print "bestCase preConditions: ", bestCase.preCons
@@ -911,18 +922,18 @@ class ModelCBR(object):
     def predict(self, worldState, action):
         
         predictionWs = WorldState()
-        predictionWs.transM = worldState.transM
-        predictionWs.quat = worldState.quat
+        predictionWs.transM = np.copy(worldState.transM)
+        predictionWs.invTrans = np.copy(worldState.invTrans)
+        predictionWs.quat = np.copy(worldState.quat)
         transformedAction = copy.deepcopy(action)
         transformedAction.transform(worldState.transM)
-        print "WorldState: ", worldState.interactionStates
         for intState in worldState.interactionStates.values():
             self.numPredictions += 1
 #            print "predicting for ", intState["intId"]
             prediction, usedCase = self.predictIntState(intState, transformedAction)
 #            print "predicted intId: ", prediction["intId"]
             predictionWs.addInteractionState(prediction, usedCase)
-        print "resulting prediction: ", predictionWs.interactionStates
+#        print "resulting prediction: ", predictionWs.interactionStates
         return predictionWs
         
     def updateState(self, state, action, prediction, result, usedCase):
@@ -1007,13 +1018,13 @@ class ModelCBR(object):
 #                print "Incorrect case new weights: ", usedCase.weights
 #        if min(predictionRating.values()) < 0.95:
         if predictionScore < PREDICTIONTHRESHOLD:
-            print "adding Case"
-            print "Prediction rating was: ", predictionRating
+#            print "adding Case"
+#            print "Prediction rating was: ", predictionRating
 #            self.cases.append(newCase)
             
             if abstractCase != None:
                 
-                print "correctAbstractCase consts: ", abstractCase.constants
+#                print "correctAbstractCase consts: ", abstractCase.constants
                 #If an abstract case is found add the reference
                 try:
                     abstractCase.addRef(newCase)
@@ -1046,6 +1057,8 @@ class ModelCBR(object):
     def update(self, worldState, action, prediction, result):
         transformedAction = copy.deepcopy(action)
         transformedAction.transform(worldState.transM)
+        if np.all(worldState.transM != result.transM):
+            raise TypeError("Wrong coordinate system!")
         #TODO Transform result state into the same coordinate system as worldState is in, otherwise block will not have changed!!!
         for intState in worldState.interactionStates.keys():
             self.updateState(worldState.interactionStates[intState], transformedAction, prediction.interactionStates[intState], 

python/conceptLearner/testsuite.py
@@ -4,38 +4,54 @@ Created on Sun Mar 22 23:16:14 2015
 TESTSUITE to try out different models
 @author: jpoeppel
 """
-from sklearn import linear_model 
-from sklearn.gaussian_process import GaussianProcess
+import numpy as np
+
+
+quatZero = np.array([0.0,0.0,0.0,1.0])
+quatRot = np.array([0.0,0.0,0.2326,0.9726])
+
+trans = np.array([-0.0123,0.7464,0.05])
+
+
+def getTransformationMatrix(quat, trans):
+    px,py,pz = trans
+    x,y,z,w = quat
+    return np.matrix([[1-2*y*y-2*z*z, 2*x*y + 2*w*z, 2*x*z - 2*w*y, px],[2*x*y-2*w*z, 1-2*x*x-2*z*z, 2*y*z+2*w*x, py],
+                          [2*x*z+2*w*y,2*y*z-2*w*x, 1-2*x*x-2*y*y, pz],[0.0,0.0,0.0,1.0]])
+                          
+def transform(pos, ori, matrix, quat):
+    tmpPos = np.matrix(np.concatenate((pos,[1])))
+    resPos = np.array((matrix*tmpPos.T)[:3]).flatten()
+
+    q1 = np.copy(quat)
+    q2 = np.copy(ori)
+    q1[:3] *= -1
+
+    newOri = np.zeros(4)
+    newOri[0] = q1[0]*q2[3]+q1[1]*q2[2]-q1[2]*q2[1]+q1[3]*q2[0]
+    newOri[1] = -q1[0]*q2[2]+q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1]
+    newOri[2] = q1[0]*q2[1]-q1[1]*q2[0]+q1[2]*q2[3]+q1[3]*q2[2]
+    newOri[3] = -q1[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]+q1[3]*q2[3]
+    newOri /= np.linalg.norm(newOri)
+    print "Q1: {},\nQ2: {},\nres: {}".format(q1,q2,newOri)
+#    tmplV = np.matrix(np.concatenate((self["linVel"],[0])))
+#    self["linVel"] = np.array((matrix*tmplV.T)[:3]).flatten()
+#    tmpaV = np.matrix(np.concatenate((self["angVel"],[0])))
+#    self["angVel"] = np.array((matrix*tmpaV.T)[:3]).flatten()
+    
+    return resPos, newOri
+    
+def getInverse(matrix):
+    invTrans = np.matrix(np.zeros((4,4)))
+    invTrans[:3,:3] = matrix[:3,:3].T
+    invTrans[:3,3] = -matrix[:3,:3].T*matrix[:3,3]
+    invTrans[3,3] = 1.0
+    return invTrans
+
 
 if __name__ == "__main__":
-
-#
-    trainData = [[0.18894831,-1.34927749,0.025, -0.4792413,0.04334731,0.],
-                 [0.18894831,-1.34927749,0.025,0.,0.,0.],
-                 [0.2,-1.4,0.025,0.,0.,0.],
-                 [0.1,-1.2,0.025,0.,0.,0.],
-                 [-0.15,1.4,0.025,0.,0.,0.]]
-    trainResultsX = [-0.29029299, 0.18894831, 0.2, 0.1,-0.15]   
-    
-    testData = [[0.2,10,0.025, 0.0,0.0,0.]]
-    
-#    
-#    trainData = [[0.18894831,-1.34927749,0.025, 0,1,0,0, -0.4792413,0.04334731,0.,1, 0.72874806,-0.82986592,0.],
-#                 [0.18894831,-1.34927749,0.025,0.,1.,0.,0.,0.,0.,0.,1,-0.4792413,0.04334731,0.],
-#                 [0.2,-1.4,0.025,0.,1.,0.,0.,0.,0.,0.,1,-0.4792413,0.04334731,0.],
-#                 [0.1,-1.2,0.025,0.,1.,0.,0.,0.,0.,0.,1,-0.4792413,0.04334731,0.],
-#                 [-0.15,1.4,0.025,0.,1.,0.,0.,0.,0.,0.,1,-0.4792413,0.04334731,0.]]
-#    trainResultsX = [-0.29029299, 0.18894831, 0.2, 0.1,-0.15]
-#    
-#    testData = [[0.18,0,0.025, 0,1,0,0, 0.0,0.0,0.,1, 0.72874806,-0.82986592,0.]]
-        
-    gp = GaussianProcess(corr='cubic')
-    gp.fit(trainData,trainResultsX)
-    print gp.predict(testData[0])
-#    clf = linear_model.SGDRegressor()
-#    for i in range(len(trainData)):
-#        clf.partial_fit([trainData[i]],[trainResultsX[i]])
-#    print clf.predict(testData[0])
-    
+    transM = getTransformationMatrix(quatRot, trans)
+    inv = getInverse(transM)
+    print transform(trans, quatRot, inv, quatRot)
     
     
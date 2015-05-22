29b63338409f0020bc2cf1d9320c733ff776da8e
Jan Pöppel <jpoeppel@techfak.uni-bielefeld.de>
2015-05-22 17:22:18 +0200

    removed doubled functions that are already present in state2

--- python/conceptLearner/state3.py
+++ python/conceptLearner/state3.py
@@ -33,57 +33,8 @@ class ObjectState(state2.ObjectState):
         if intState["contact"]:
             self["contact"] = intState["sname"]
             
-class WorldState(object):
+class WorldState(state2.WorldState):
     
-    def __init__(self, transM = None, invTrans = None, ori = None):
-        self.objectStates = {}
-        self.interactionStates = {}
-        self.numIntStates = 0
-        self.predictionCases = {}
-        self.transM = transM
-        self.invTrans = invTrans
-        self.ori = ori
-
-    def addInteractionState(self, intState, usedCase = None):
-#        print "adding interactionState: ", intState["intId"]
-        assert isinstance(intState, InteractionState), "{} (intState) is not an InteractionState object.".format(intState)
-        self.interactionStates[intState["intId"]] = intState
-        self.numIntStates += 1        
-        self.predictionCases[intState["intId"]] = usedCase
-    
-    def parseModels(self, models):
-        
-        for m in models:
-            if m.name == "ground_plane" or "wall" in m.name or "Shadow" in m.name:
-                #Do not model the ground plane or walls for now
-                continue
-            else:
-#                print "parsing: ", m.name
-                tmp = ObjectState()
-                tmp["pos"] = np.round(np.array([m.pose.position.x,m.pose.position.y,m.pose.position.z]), NUMDEC) #/ 2.0
-#                print "parsing model: {}, pos: {}".format(m.name, tmp["pos"])
-                tmp["euler"]  = np.round(common.quaternionToEuler(np.array([m.pose.orientation.x,m.pose.orientation.y,
-                                            m.pose.orientation.z,m.pose.orientation.w])), NUMDEC)
-                tmp["euler"][:2] = 0
-#                tmp["euler"]  = common.quaternionToEuler(np.array([0.0,0.0,m.pose.orientation.z,m.pose.orientation.w]))
-#                print "quaternion: {}, euler: {}".format(np.array([m.pose.orientation.x,m.pose.orientation.y,
-#                                            m.pose.orientation.z,m.pose.orientation.w]), tmp["euler"])
-                tmp["linVel"] = np.round(np.array([m.linVel.x,m.linVel.y,m.linVel.z]), NUMDEC)
-                if np.linalg.norm(tmp["linVel"]) < 0.01:
-                    tmp["linVel"] = np.array([0.0,0.0,0.0])
-#                print "name: {}, linVel: {}".format(m.name, tmp["linVel"])
-                tmp["angVel"] = np.round(np.array([m.angVel.x,m.angVel.y,m.angVel.z]), 1)
-#                print "Raw AngVel: ", tmp["angVel"]
-                tmp["name"] = m.name
-                tmp["id"] = m.id
-                tmp["type"] = m.type
-                self.objectStates[m.name] = tmp
-                
-                if m.name == "blockA" and self.transM == None:
-                    self.transM = common.eulerPosToTransformation(tmp["euler"],tmp["pos"])
-                    self.invTrans = common.invertTransMatrix(self.transM)
-#                    print "invTrans: ", self.invTrans
-                    self.ori = np.copy(tmp["euler"])
 
                 
     def parseInteractions(self):
@@ -99,23 +50,8 @@ class WorldState(object):
             if not np.array_equal(o1,gripperO):             
                 intState.fill(o1)
                 self.addInteractionState(intState)
-#                    
-
+                   
                 
-    def parseContacts(self, contacts):
-        for c in contacts:
-            o1Name = c.wrench[0].body_1_name.split(':')[0]
-            o2Name = c.wrench[0].body_2_name.split(':')[0]
-            if self.objectStates.has_key(o1Name):
-                self.objectStates[o1Name]["contact"] = o2Name
-            if self.objectStates.has_key(o2Name):
-                self.objectStates[o2Name]["contact"] = o1Name
-    
-    def parse(self, gzWS):
-        self.parseModels(gzWS.model_v.models)
-        self.parseContacts(gzWS.contacts.contact)
-        self.parseInteractions()
-        
     def reset(self, worldState):
         self.objectStates = {}
         self.interactionStates = {}
@@ -144,9 +80,3 @@ class WorldState(object):
         self.parseInteractions()
 #        
 #        print "InteractionStates: ", self.interactionStates.values()
-
-    def getInteractionState(self, sname):
-        for i in self.interactionStates.values():
-            if i["sname"] == sname:
-                return i
-        return None    

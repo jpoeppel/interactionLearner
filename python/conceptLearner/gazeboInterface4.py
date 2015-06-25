hopefully fixed error in distance calculation in state2, state4 uses a new, better distance calculation that should be ported to state2 in the future if state2 is reused. Other than that worked on getting state4 to work with the Actions model. It does now run, but the action selection is still lacking.

python/conceptLearner/gazeboInterface4.py
@@ -28,10 +28,8 @@ from config import DIFFERENCES, SINGLE_INTSTATE, INTERACTION_STATES
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
-if INTERACTION_STATES:
-    import model4 as model
-else:
-    import modelActions as model
+
+import modelActions as model
 #import model6 as model
 
 from sklearn.externals.six import StringIO
@@ -62,7 +60,7 @@ DIRECTIONGENERALISATION = False
 
 
 
-NUM_TRAIN_RUNS = 20
+NUM_TRAIN_RUNS = 5
 NUM_TEST_RUNS = 50
 
 class GazeboInterface():
@@ -77,8 +75,9 @@ class GazeboInterface():
         self.worldModel = model.ModelAction()
         self.lastAction = model.GripperAction()
         self.lastPrediction = None
-        
+        self.ignore = True
         self.target = None
+        self.startup = True
         
         self.trainRun = 0
         self.testRun = 0
@@ -128,6 +127,7 @@ class GazeboInterface():
                     
         
         
+        
                           
         while self.active:
             yield From(trollius.sleep(1))
@@ -159,6 +159,7 @@ class GazeboInterface():
         """
         Function to send a stop command to gazebo to stop the gripper from moving.
         """
+        print "sending stop command"
         msg = pygazebo.msg.gripperCommand_pb2.GripperCommand()
         msg.cmd = GAZEBOCMDS["MOVE"]
         msg.direction.x = 0.0
@@ -246,29 +247,34 @@ class GazeboInterface():
         data: bytearry
             Protobuf bytearray containing a list of models
         """
-        worldState = worldState_pb2.WorldState.FromString(data)
-#        if self.lastPrediction != None:
-##            print "Parsing worldState with last coordinate system."
-##            resultWS = model.WorldState(self.lastPrediction.transM, self.lastPrediction.invTrans, self.lastPrediction.quat)
-#            resultWS = model.WorldState(self.lastPrediction.transM, self.lastPrediction.invTrans, self.lastPrediction.ori)
-#            resultWS.parse(worldState)
-#        else:
-#            resultWS = None
-#        print "parsing new WorldState"
-        newWS = model.WorldState()
-        newWS.parse(worldState)
-        
-        if MODE == FREE_EXPLORATION:
-            self.randomExploration(newWS)
-        elif MODE == PUSHTASK:
-            self.pushTask(newWS)
-        elif MODE == PUSHTASKSIMULATION:
-            self.pushTaskSimulation(newWS)
-        elif MODE == MOVE_TO_TARGET:
-            self.getTarget()
-            self.moveToTarget(newWS)
+        if self.startup:
+            self.resetWorld()
+            self.startup= False
+            
         else:
-            raise AttributeError("Unknown MODE: ", MODE)
+            worldState = worldState_pb2.WorldState.FromString(data)
+    #        if self.lastPrediction != None:
+    ##            print "Parsing worldState with last coordinate system."
+    ##            resultWS = model.WorldState(self.lastPrediction.transM, self.lastPrediction.invTrans, self.lastPrediction.quat)
+    #            resultWS = model.WorldState(self.lastPrediction.transM, self.lastPrediction.invTrans, self.lastPrediction.ori)
+    #            resultWS.parse(worldState)
+    #        else:
+    #            resultWS = None
+    #        print "parsing new WorldState"
+            newWS = model.WorldState()
+            newWS.parse(worldState)
+            
+            if MODE == FREE_EXPLORATION:
+                self.randomExploration(newWS)
+            elif MODE == PUSHTASK:
+                self.pushTask(newWS)
+            elif MODE == PUSHTASKSIMULATION:
+                self.pushTaskSimulation(newWS)
+            elif MODE == MOVE_TO_TARGET:
+                self.getTarget()
+                self.moveToTarget(newWS)
+            else:
+                raise AttributeError("Unknown MODE: ", MODE)
 
 
     def runEnded(self, worldState):
@@ -309,6 +315,7 @@ class GazeboInterface():
         if RANDOM_BLOCK_ORI:
             self.sendPose("blockA", np.array([0.0, 0.5, 0.05]) , np.random.rand()-0.5)
         self.stepCounter = 0
+        self.ignore = True
         
     def startRun2(self, randomRange=0.5):
         self.runStarted = True
@@ -317,11 +324,13 @@ class GazeboInterface():
         self.sendPose("gripper", np.array([0.0,posy,0.03]), 0.0)
         self.sendPose("blockA", np.array([-0.5, 0.0, 0.05]) , 1.0)
         self.stepCounter = 0
+        self.ignore = True
         
     def startRun3(self):
         self.runStarted = True
         self.sendPose("gripper", np.array([-0.5, 0.5,0.03]), -1.0)
         self.stepCounter = 0
+        self.ignore = True
         
 
     def updateTmpErrors(self, worldState):

python/conceptLearner/metrics.py
@@ -24,7 +24,7 @@ def cosS(a, b):
         print "b: " + str(b)
     
 def expS(a,b):
-    return math.exp(-10*(np.linalg.norm(a-b)))
+    return math.exp(-0.5*(np.linalg.norm(a-b)))
     
 def equalS(a,b):
     if a == b:
@@ -50,7 +50,8 @@ similarities = {"dir": expS, "linVel": expS, "orientation": expS, "pos": expS,
            "dangVel": expS, "mvDir": expS, "otype": equalS, "sangVel":expS, "oangVel": expS, 
            "sori": expS, "spos": expS, "opos": expS, "stype":equalS, "sid": equalS, 
            "slinVel": expS, "olinVel":expS, "oname": equalS, "euler": expS, "seuler": expS, 
-           "deuler": expS, "oeuler": expS, "side": equalS}
+           "deuler": expS, "oeuler": expS, "side": equalS, "relPosX": expS, "relPosY": expS,
+           "relPosZ": expS, "closing" : expS, "closingDivDist": expS, }
            
 def cosD(a,b):
     return 1-cosS(a,b)

python/conceptLearner/modelActions.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jun 22 12:41:59 2015
-
+TODO: When using more then 2 objects, the actions need to differentiate which interactionState
+they primarily use!!
 @author: jpoeppel
 """
 
@@ -50,7 +51,8 @@ class BaseCase(object):
         return r
         
     def getSetOfActionAttribs(self):
-        
+#        if self.preState["name"] == "blockA":
+#            print "dif: ", self.dif
         r = Set()
         for k,v in self.dif.items():
             if k in self.preState.actionItems and np.linalg.norm(v) > 0.01:
@@ -76,27 +78,33 @@ class Action(object):
         self.targets = Set()
         self.effect = {}
         self.refCases = []
+        self.vecs = []
         self.unusedFeatures = []
         for k in variables:
             self.effect[k] = ITM()
         
-    def applyAction(self, state, worldState, strength = 1.0):
-        for k,v in self.effect.items():
-            if isinstance(v, ITM):
-                state[k] += strength * v.predict(worldState.toVec(self.unusedFeatures))
-            else:
-                state[k] += strength * v
+    def applyAction(self, state, intStates, strength = 1.0):
+        for intState in intStates:
+            for k,v in self.effect.items():
+                if isinstance(v, ITM):
+                    state[k] += strength * v.predict(intState.getVec())
+                else:
+                    state[k] += strength * v
             
-    def rate(self, objectState, worldState):
+    def rate(self, objectState, intStates):
 #        print "rating action: {}, precons: {}".format(self.effect.keys(), self.preConditions)
         if objectState["name"] not in self.targets:
             return 0
-            
-        r = {}
-        s = 0.0
-        for k,v in self.preConditions.items():
-             s += similarities[k](v,worldState[k])
-             r[k] = similarities[k](v,worldState[k])
+        print "rate: ", intStates
+        bestScore = 0.0
+        for intState in intStates:
+            r = {}
+            s = 0.0
+            for k,v in self.preConditions.items():
+                 s += similarities[k](v,intState[k])
+                 r[k] = similarities[k](v,intState[k])
+            if s > bestScore:
+                bestScore = s
              
         print "action: {} got rating: {} for {}".format(self.effect.keys(), r, objectState["name"])
         if len(self.preConditions) != 0:
@@ -104,23 +112,39 @@ class Action(object):
         else:
             return 1.0
             
-    def update(self, case, worldState):
+    def rate2(self, objectState, intStates):
+        if objectState["name"] not in self.targets:
+            return 0
+        bestScore = 0.0
+        print "intStates: ", intStates
+        for intState in intStates:
+            
+            for vec in self.vecs:
+                s = np.exp(-0.5*np.linalg.norm(vec-intState.getVec()))
+                if s > bestScore:
+                    bestScore = s
+                    
+        return bestScore
+            
+    def update(self, case, intStates):
         self.targets.add(case.preState["name"])
-        for k,v in worldState.items():
-            if k in self.preConditions:
-                if np.linalg.norm(v-self.preConditions[k]) > 0.1:
-                    del self.preConditions[k]
-            else:
-                if len(self.refCases) < 1:
-                    self.preConditions[k] = v 
+        for intState in intStates:
+            self.vecs.append(intState.getVec())
+#            for k,v in intState.relItems():
+#                if k in self.preConditions:
+#                    if np.linalg.norm(v-self.preConditions[k]) > 0.1:
+#                        del self.preConditions[k]
+#                else:
+#                    if len(self.refCases) < 1:
+#                        self.preConditions[k] = v 
         for k,v in self.effect.items():
 #            if not k in self.effect:
 #                self.effect[k] = ITM()
-            v.train(Node(0, wIn=worldState.toVec(self.unusedFeatures), wOut=case.dif[k]))
+            v.train(Node(0, wIn=intState.getVec(), wOut=case.dif[k]))
                 
-        self.refCases.append((case, worldState))
+        self.refCases.append((case, intStates))
         
-        
+
     @classmethod
     def getGripperAction(cls, cmd=GZCMD["NOTHING"] , direction=np.zeros(3)):
         res = GripperAction(cmd, direction)
@@ -150,9 +174,12 @@ class Predictor(object):
         self.unusedFeatures = []
         
     def predict(self, state):
-        res = copy.deepcopy(state)
+        res = ObjectState.clone(state)
+#        print "predict state: {} \n res: {}".format(state, res)
 #        prediction = self.pred.predict(state.toVec())
 #        print "prediction for state: {}: {}".format(state["name"], prediction)
+#        if state["name"] == "blockA":
+#            print "predicting block A with: ", state
         i = 0
         for k in state.relKeys:
 #            if hasattr(res[k], "__len__"):
@@ -161,13 +188,18 @@ class Predictor(object):
 #                l = 1
 #            res[k] += prediction[i:i+l]
 #            i += l
-            res[k] += self.pred[k].predict(state.toVec(self.unusedFeatures))
+            res[k] += self.pred[k].predict(state.getVec())
             
-        print "Prediction: ", res
+#        if state["name"] == "blockA":
+#            print "Prediction: ", res
         return res
         
-    def update(self, case):
-        print "updating predictor for ", self.targets
+    def update(self, case, action, worldState):
+#        print "updating predictor for ", self.targets
+        preState = ObjectState.clone(case.preState)
+        action.applyAction(preState, worldState.getInteractionStates(preState["name"]))
+#        if case.preState["name"] == "blockA":
+#            print "updating with: pre: {} \n post: {}".format(preState, case.postState)
         if len(self.refCases) == 0:
             for k in case.preState.relKeys:
                 self.pred[k] = ITM()
@@ -175,7 +207,7 @@ class Predictor(object):
 #            raise AttributeError("Case already present")
         self.refCases.append(case)
         for k in case.preState.relKeys:
-            self.pred[k].train(Node(0, wIn=case.preState.toVec(self.unusedFeatures), 
+            self.pred[k].train(Node(0, wIn=preState.getVec(), 
                              wOut=case.dif[k]))
 #        self.pred.train(Node(0, wIn=case.preState.toVec(self.unusedFeatures), 
 #                             wOut = case.postState.toVec()-case.preState.toVec()))   
@@ -195,17 +227,18 @@ class ModelAction(object):
         if objectState["name"] == "gripper":
             objectState["linVel"] = action["mvDir"]
 #        print "rating for: ", objectState["name"]
-        scoreList = [(a.rate(objectState, worldState), a) for a in self.actions]
+        scoreList = [(a.rate2(objectState, worldState.getInteractionStates(objectState["name"])), a) for a in self.actions]
         sortedList = sorted(scoreList, key=itemgetter(0), reverse=True) 
-#        print "scorelist for {}: {}".format(objectState["name"], sortedList)
-        filteredList = filter(lambda x: x[0] > 0.9, sortedList)
+        print "scorelist for {}: {}".format(objectState["name"], sortedList)
+        filteredList = filter(lambda x: x[0] > 0.75, sortedList)
 #        print "filteredList for {}: {}".format(objectState["name"], filteredList)
         totalScore = np.sum([s[0] for s in filteredList])
         if totalScore == 0:
             totalScore = 1
-        res = copy.deepcopy(objectState)
+        res = ObjectState.clone(objectState)
         for s,a in filteredList:
-            a.applyAction(res, worldState, s/totalScore)
+            a.applyAction(res, worldState.getInteractionStates(res["name"]), s/totalScore)
+            
         return res
         
     def predictObjectState(self, objectState):
@@ -215,7 +248,7 @@ class ModelAction(object):
                 return pred.predict(objectState)
                 
         #Return the state itself if no predictor was found
-        return copy.deepcopy(objectState)
+        return ObjectState.clone(objectState)
     
     def predict(self, worldState, action):
         print "predict"
@@ -229,6 +262,8 @@ class ModelAction(object):
             newOS = self.predictObjectState(newOS)
             objectState = newOS
             resultWS.addObjectState(newOS)
+            print "prediction interactions"
+            resultWS.parseInteractions()
             
         return resultWS
         
@@ -236,13 +271,15 @@ class ModelAction(object):
         for a in self.actions:
             if case.preState["name"] in a.targets:
                 if Set(a.effect.keys()) == case.getSetOfActionAttribs():
-                    a.update(case, worldState)
-                    return
+                    a.update(case, worldState.getInteractionStates(case.preState["name"]))
+                    return a
+
         #If no action was found
         print "creating new action for {}: {}".format(case.preState["name"], case.getSetOfActionAttribs())
         newAction = Action(case.getSetOfActionAttribs())
-        newAction.update(case, worldState)
+        newAction.update(case, worldState.getInteractionStates(case.preState["name"]))
         self.actions.append(newAction)
+        return newAction
         
         
     def updateState(self, objectState, worldState, action, resultingOS):
@@ -250,21 +287,20 @@ class ModelAction(object):
         predictionRating = resultingOS.score(objectState)
         print "Prediction rating: ", predictionRating
         if predictionRating < THRESHOLD:            
-            self.checkForAction(case, worldState)
+            responsibleAction = self.checkForAction(case, worldState)
             predFound = False
             for pred in self.predictors:
                 if objectState["name"] in pred.targets:
-                    pred.update(case)
+                    pred.update(case, responsibleAction, worldState)
                     
                     predFound = True
             if not predFound:
                 pred = Predictor()
                 pred.targets.add(objectState["name"])
-                pred.update(case)
+                pred.update(case, responsibleAction, worldState)
                 self.predictors.append(pred)
             self.cases.append(case)
     
     def update(self, worldState, action, prediction, result):
-        print "Update!: ", prediction.objectStates
         for os in prediction.objectStates.values():
             self.updateState(os, worldState, action, result.getObjectState(os["name"]))

python/conceptLearner/state2.py
@@ -480,7 +480,7 @@ class InteractionState(State):
         x2yn = x2x*s + x2y*c
         x1n = np.array([x1xn,x1yn,0]) + mp
         x2n = np.array([x2xn,x2yn,0]) + mp
-        if x0x <= x2x and x0x >= x1x: 
+        if x0x <= x2xn and x0x >= x1xn: 
             d1 = abs((x2n[0]-x1n[0])*(x1n[1]-x0y)-(x1n[0]-x0x)*(x2n[1]-x1n[1]))/math.sqrt((x2n[0]-x1n[0])**2+(x2n[1]-x1n[1])**2) - 0.025
         else:
             d1 = min(np.linalg.norm(x1n-np.array([x0x,x0y,x0z])), np.linalg.norm(x2n-np.array([x0x,x0y,x0z])))
@@ -494,7 +494,7 @@ class InteractionState(State):
         x2yn = x2x*s + x2y*c
         x1n = np.array([x1xn,x1yn,0]) + mp
         x2n = np.array([x2xn,x2yn,0]) + mp
-        if x0x <= x2x and x0x >= x1x: 
+        if x0x <= x2xn and x0x >= x1xn: 
             d2 = abs((x2n[0]-x1n[0])*(x1n[1]-x0y)-(x1n[0]-x0x)*(x2n[1]-x1n[1]))/math.sqrt((x2n[0]-x1n[0])**2+(x2n[1]-x1n[1])**2) - 0.025
         else:
             d2 = min(np.linalg.norm(x1n-np.array([x0x,x0y,x0z])), np.linalg.norm(x2n-np.array([x0x,x0y,x0z])))
@@ -508,7 +508,7 @@ class InteractionState(State):
         x2yn = x2x*s + x2y*c
         x1n = np.array([x1xn,x1yn,0]) + mp
         x2n = np.array([x2xn,x2yn,0]) + mp
-        if x0y <= x1y and x0y >= x2y: 
+        if x0y <= x1yn and x0y >= x2yn: 
             d3 = abs((x2n[0]-x1n[0])*(x1n[1]-x0y)-(x1n[0]-x0x)*(x2n[1]-x1n[1]))/math.sqrt((x2n[0]-x1n[0])**2+(x2n[1]-x1n[1])**2) - 0.025
         else:
             d3 = min(np.linalg.norm(x1n-np.array([x0x,x0y,x0z])), np.linalg.norm(x2n-np.array([x0x,x0y,x0z])))
@@ -522,7 +522,7 @@ class InteractionState(State):
         x2yn = x2x*s + x2y*c
         x1n = np.array([x1xn,x1yn,0]) + mp
         x2n = np.array([x2xn,x2yn,0]) + mp
-        if x0y <= x1y and x0y >= x2y: 
+        if x0y <= x1yn and x0y >= x2yn: 
             d4 = abs((x2n[0]-x1n[0])*(x1n[1]-x0y)-(x1n[0]-x0x)*(x2n[1]-x1n[1]))/math.sqrt((x2n[0]-x1n[0])**2+(x2n[1]-x1n[1])**2) - 0.025
         else:
             d4 = min(np.linalg.norm(x1n-np.array([x0x,x0y,x0z])), np.linalg.norm(x2n-np.array([x0x,x0y,x0z])))

python/conceptLearner/state4.py
@@ -10,6 +10,7 @@ import numpy as np
 import common
 from common import NUMDEC
 import math
+import copy
 
 WIDTH = {"blockA":0.25} #Width from the middle point
 HEIGHT = {"blockA":0.05} #Height from the middle point
@@ -30,6 +31,10 @@ class State(dict):
     def score(self, other):
         return np.exp(-0.5*np.linalg.norm(self.getVec()-other.getVec()))
         
+    def relItems(self):
+        for k in self.relKeys:
+            yield k, self[k]
+        
     
 class ObjectState(State):
     
@@ -42,7 +47,16 @@ class ObjectState(State):
                      "pos": self.vec[1:4], "linVel": self.vec[5:8], "contact":None})
         self.mask = np.array(range(len(self.vec)))
         self.relKeys = ["id", "posX", "posY", "posZ", "ori", "linVelX", "linVelY", "linVelZ", "angVel"]
-        self.actionItems = ["linVel", "angVel"]
+        self.actionItems = ["linVelX", "linVelY", "linVelZ", "angVel"]
+        
+    @classmethod
+    def clone(cls, other):
+        assert isinstance(other, ObjectState), "{} is no ObjectState (but {})".format(other, type(other))
+        res = cls()
+        np.copyto(res.vec, other.vec)
+        res["name"] = other["name"]
+        res["contact"] = copy.deepcopy(other["contact"])
+        return res
         
 class InteractionState(State):
     
@@ -53,7 +67,7 @@ class InteractionState(State):
                      "relPosX": self.vec[5:6], "relPosY": self.vec[6:7], "relPosZ":self.vec[7:8],
                      "closingDivDist":self.vec[8:9], "relPos": self.vec[5:8] })
         self.mask = np.array(range(len(self.vec)))
-
+        self.relKeys = ["sid", "oid", "dist", "closing", "contact", "relPosX", "relPosY", "relPosZ", "closingDivDist"]
         
 class WorldState(object):
     
@@ -79,10 +93,21 @@ class WorldState(object):
                 tmp["posZ"][0] = np.round(m.pose.position.z, NUMDEC)
                 tmp["ori"][0] = np.round(common.quaternionToEuler(np.array([m.pose.orientation.x,m.pose.orientation.y,
                                             m.pose.orientation.z,m.pose.orientation.w])), NUMDEC)[2]
+                print "linVel before setting: ", tmp["linVel"]
                 tmp["linVelX"][0] = np.round(m.linVel.x, NUMDEC)
                 tmp["linVelY"][0] = np.round(m.linVel.y, NUMDEC)
                 tmp["linVelZ"][0] = np.round(m.linVel.z, NUMDEC)
+                print "linVel: ", tmp["linVel"]
+                print "norm linVel: ", np.linalg.norm(tmp["linVel"])
+                if np.linalg.norm(tmp["linVel"]) < 0.01:
+                    print "setting linVel to 0"
+                    tmp["linVelX"][0] = 0.0
+                    tmp["linVelY"][0] = 0.0
+                    tmp["linVelZ"][0] = 0.0
                 tmp["angVel"][0] = np.round(m.angVel.z, NUMDEC)
+#                if m.name == "blockA":
+                
+#                    print "pos: ", tmp["pos"]
                 self.objectStates[m.name] = tmp
                 
     def parseContacts(self, contacts):
@@ -111,77 +136,97 @@ class WorldState(object):
                     intState["dist"][0], intState["closing"][0] = self.computeDistanceClosing(os1,os2)
                     if os1["contact"] == n2:
                         intState["contact"][0] = 1
-                    intState["relPosX"][0] = os1["posX"]-os2["posX"]
-                    intState["relPosY"][0] = os1["posY"]-os2["posY"]
-                    intState["relPosZ"][0] = os1["posZ"]-os2["posZ"]
-                    intState["closingDivDist"][0] = intState["closing"]/intState["dist"]
+                    intState["relPosX"][0] = np.round(os1["posX"]-os2["posX"], NUMDEC)
+                    intState["relPosY"][0] = np.round(os1["posY"]-os2["posY"], NUMDEC)
+                    intState["relPosZ"][0] = np.round(os1["posZ"]-os2["posZ"], NUMDEC)
+                    if intState["dist"] != 0:
+                        intState["closingDivDist"][0] = intState["closing"]/intState["dist"]
+                    else:
+                        intState["closingDivDist"][0] = intState["closing"]/0.001
                     self.interactionStates[intState["name"]] = intState
             
             
     def parse(self, gzWS):
         self.parseModels(gzWS.model_v.models)
         self.parseContacts(gzWS.contacts)
+        print "parsing"
         self.parseInteractions()
         
-    def calcDist(self, x0x,x0y,mp, x1x,x1y,x2x,x2y,c,s):
+        
+    def calcDist(self, p, mp, x1x,x1y,x2x,x2y,c,s):
         x1xn = x1x*c - x1y*s
         x1yn = x1x*s + x1y*c
         x2xn = x2x*c - x2y*s
         x2yn = x2x*s + x2y*c
-        x1n = np.array([x1xn,x1yn]) + mp
-        x2n = np.array([x2xn,x2yn]) + mp
-        if x0x <= x2x and x0x >= x1x: 
-            d = abs((x2n[0]-x1n[0])*(x1n[1]-x0y)-(x1n[0]-x0x)*(x2n[1]-x1n[1]))/math.sqrt((x2n[0]-x1n[0])**2+(x2n[1]-x1n[1])**2) - 0.025
-        else:
-            d = min(np.linalg.norm(x1n-np.array([x0x,x0y])), np.linalg.norm(x2n-np.array([x0x,x0y])))
-        return d
+        v = np.array([x1xn,x1yn]) + mp
+        w = np.array([x2xn,x2yn]) + mp
+#        print "x1xn: {}, x1yn: {}, x2xn: {}, x2yn:{}".format(x1xn,x1yn,x2xn,x2yn)
+#        print "x1x: {}, x1y: {}, x2x: {}, x2y: {}".format(x1x,x1y,x2x,x2y)
+#        print "mp: ", mp
+        l2 = np.dot(v-w, v-w)
+#        print "l2: ", l2
+        if l2 == 0.0:
+            return np.sqrt(np.dot(v-p, v-p)), v
+        t = np.dot(p-v, w-v) / l2
+        if t < 0.0:
+            return np.sqrt(np.dot(v-p,v-p)), v
+        elif t > 1.0:
+            return np.sqrt(np.dot(w-p,w-p)), w
+        projection = v + t * (w - v)
+        return np.sqrt(np.dot(p-projection, p-projection)), projection
         
     def computeDistanceClosing(self, os1, os2):
+#        print "ComputeDistanceClosing: os1vel: {}, os2vel: {}".format(os1["linVel"], os2["linVel"])
         if os1["name"] == "gripper":
-            x0x,x0y = os1["pos"][:2]
+            p = os1["pos"][:2]
             mp = np.copy(os2["pos"][:2])
             ang = os2["ori"]
             blockN = os2["name"]
-            vel = os2["linVel"]
+            vel = os2["linVel"]-os1["linVel"]
         elif os2["name"] == "gripper":
-            x0x,x0y = os2["pos"][:2]
+            p = os2["pos"][:2]
             mp = np.copy(os1["pos"][:2])
             ang = os1["ori"]
             blockN = os1["name"]
-            vel = os1["linVel"]
+            vel = os2["linVel"]-os1["linVel"]
         else:
             raise NotImplementedError("Currently only distance between gripper and object is implemented.")
-        c = np.cos(ang)
-        s = np.sin(ang)
+        c = math.cos(ang)
+        s = math.sin(ang)
         
         x1x = WIDTH[blockN]
         x1y = HEIGHT[blockN]
         x2x = x1x
         x2y = -x1y
-        d1 = self.calcDist(x0x,x0y,mp,x1x,x1y,x2x,x2y,c,s)
+        d1, p1 = self.calcDist(p,mp,x1x,x1y,x2x,x2y,c,s) 
         
         x1x = WIDTH[blockN]
         x1y = HEIGHT[blockN]
         x2x = -x1x
         x2y = x1y
-        d2 = self.calcDist(x0x,x0y,mp,x1x,x1y,x2x,x2y,c,s)
+        d2, p2 = self.calcDist(p,mp,x1x,x1y,x2x,x2y,c,s)
         
         x1x = -WIDTH[blockN]
         x1y = HEIGHT[blockN]
         x2x = x1x
         x2y = -x1y
-        d3 = self.calcDist(x0x,x0y,mp,x1x,x1y,x2x,x2y,c,s)
+        d3, p3 = self.calcDist(p,mp,x1x,x1y,x2x,x2y,c,s)
         
         x1x = WIDTH[blockN]
         x1y = -HEIGHT[blockN]
         x2x = -x1x
         x2y = x1y
-        d4 = self.calcDist(x0x,x0y,mp,x1x,x1y,x2x,x2y,c,s)
+        d4, p4 = self.calcDist(p,mp,x1x,x1y,x2x,x2y,c,s)
         
         ds = [d1,d2,d3,d4]
+        ps = [p1,p2,p3,p4]
         di = np.argmin(ds)
-        normal = np.array([np.cos(ang+di*(math.pi/2.0)), np.sin(ang+di*(math.pi/2.0)), 0])
-        return ds[di], np.dot(normal, vel)
+        normal = np.zeros(3)
+        normal[:2] = (p-ps[di])/np.linalg.norm(ps[di])
+#        print "normal: ", normal
+#        print "vel: ", vel
+#        print "norm vel: ", np.linalg.norm(vel)
+        return ds[di]-0.025, np.dot(normal, vel)
         
     def getInteractionState(self, name):
         return self.interactionStates[name]
@@ -191,4 +236,7 @@ class WorldState(object):
         
     def addObjectState(self, os):
         self.objectStates[os["name"]] = os
+        
+    def getInteractionStates(self, sname):
+        return [intState for intState in self.interactionStates.values() if intState["sname"] == sname]
             

python/conceptLearner/testsuite.py
@@ -5,7 +5,7 @@ TESTSUITE to try out different models
 @author: jpoeppel
 """
 import numpy as np
-
+import copy
 
 quatZero = np.array([0.0,0.0,0.0,1.0])
 quatRot = np.array([0.0,0.0,0.2326,0.9726])
@@ -48,13 +48,43 @@ def getInverse(matrix):
     invTrans[3,3] = 1.0
     return invTrans
 
+class dummy(object):
+    
+    def __init__(self):
+        self.vec = np.array(range(10))
+        self.a = self.vec[0:1]
+        self.b = self.vec[0:3]
+
 
 if __name__ == "__main__":
-    transM = getTransformationMatrix(quatRot, trans)
-    inv = getInverse(transM)
-    a,b = transform(trans, quatRot, inv, quatRot)
-    q = quatRot
-    q[:3] *= -1
-    print transform(a+np.array([0.0,0.1,0.0]), b, transM, q)
-    
-    
+#    transM = getTransformationMatrix(quatRot, trans)
+#    inv = getInverse(transM)
+#    a,b = transform(trans, quatRot, inv, quatRot)
+#    q = quatRot
+#    q[:3] *= -1
+#    print transform(a+np.array([0.0,0.1,0.0]), b, transM, q)
+    c = dummy()
+    print "c.vec: ", c.vec
+    print "c.a: ", c.a
+    print "c.b: ", c.b
+    d = copy.deepcopy(c)
+    e = dummy()
+    e.vec = np.copy(c.vec)
+    print "d.vec: ", d.vec
+    print "d.a: ", d.a
+    print "d.b: ", d.b
+    c.a += 10
+    print "c.vec: ", c.vec
+    print "c.a: ", c.a
+    print "c.b: ", c.b
+    d.a += 20
+    print "d.vec: ", d.vec
+    print "d.a: ", d.a
+    print "d.b: ", d.b
+    e.a += 30
+    print "e.vec: ", e.vec
+    print "e.a: ", e.a
+    print "e.b: ", e.b
+    print "c.vec: ", c.vec
+    print "c.a: ", c.a
+    print "c.b: ", c.b
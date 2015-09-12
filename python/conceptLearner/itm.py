2df99844d8ae3d7b9fca81eb8f3ebfe596e501d8
Jan Pöppel <jpoeppel@techfak.uni-bielefeld.de>
2015-09-13 01:29:04 +0200

    Update itm.py
    Implemented hopefully correct itm. Needs speed comparison to old itm!

--- python/conceptLearner/itm.py
+++ python/conceptLearner/itm.py
@@ -7,6 +7,7 @@ Created on Fri Sep 11 16:06:08 2015
 """
 
 import numpy as np
+import collections
 
 EMAX = 0.001
 
@@ -15,7 +16,7 @@ class Node(np.ndarray):
     def __new__(cls, input_array, id=-1, output = np.zeros(1)):
         obj = np.asarray(input_array).view(cls)
         obj.out = output
-        obj.A = np.zeros((len(input_array),len(output)))
+        obj.A = np.zeros((len(output),len(input_array)))
         obj.neig = {}
         obj.id = id
         return obj
@@ -39,28 +40,28 @@ class Node(np.ndarray):
 class ITM(object):
     
     def __init__(self):
-        self.nodes = {}
-        self.edges = {}
+        self.nodes = collections.OrderedDict()
+#        self.nodes = np.zeros # Try storing all nodes in one nparray that needs to be reshaped
         
         
     def update(self, x, y, etaIn=0.0, etaOut=0.0, etaA=0.0):
         #Get winners:
         if len(self.nodes) > 1:
-            numpyVals = np.array(self.nodes.values())
+            numpyVals = Node(self.nodes.values())-x
             sortedIndices = np.argsort(np.linalg.norm(numpyVals, axis=1))
-            w = numpyVals[sortedIndices[0]]
+            w = self.nodes[sortedIndices[0]]
             wI =  w.id
-            s = numpyVals[sortedIndices[1]]
+            s = self.nodes[sortedIndices[1]]
             sI = s.id
             #Adapt winner
             dwIn = etaIn*(x-w)
             w += dwIn
             w.out += etaOut*(y-w.out+np.dot(w.A,x-w)) + np.dot(w.A,dwIn)
-            w.A = etaA*np.dot((y-w.out+np.dot(w.A,x-w)), (x-w)/np.linalg.norm(x-w))
+            w.A = etaA*np.outer((y-w.out+np.dot(w.A,x-w)), (x-w)/np.linalg.norm(x-w))
             
             #Add edge
-            w.addNeighbour(sI)
-            s.addNeighbour(wI)
+            w.addNeighbour(sI,s)
+            s.addNeighbour(wI,w)
             #Check neighbours
             for nI, n in w.neig.items():
                 if np.dot(w-s,n-s) < 0:
@@ -76,14 +77,13 @@ class ITM(object):
                 w.addNeighbour(nI, n)
                 n.addNeighbour(wI, w)
             
-            if np.linalg.norm(w,s) < 0.5*EMAX:
+            if np.linalg.norm(w-s) < 0.5*EMAX:
                 if len(self.nodes) > 2:
                     self.deleteNode(sI)             
         else:
             #To few nodes
             nI = len(self.nodes)
             self.nodes[nI] = Node(x,nI,y)
-            self.edges[nI] = []
         pass
     
     def deleteNode(self, nodeId):
@@ -94,11 +94,12 @@ class ITM(object):
     
     def test(self, x):
 #        sortedNodes = np.sort(np.linalg.norm(np.array(self.nodes.values()), axis=1))
-        sortedNodes = np.sort(self.nodes.values())
-        return sortedNodes[0].out
+        numpyVals = Node(self.nodes.values())-x
+        sortedIndices = np.argsort(np.linalg.norm(numpyVals, axis=1))
+        return self.nodes[sortedIndices[0]].out
     
 if __name__ == "__main__":
-    a = [1,2,3]
+    a = np.array([1,2,3])
     b = [4,5,6]
     n = Node(a)
     n2 = Node(b)
@@ -108,3 +109,14 @@ if __name__ == "__main__":
     print c[1]
     print type(c)
     print type(c[0])
+    n3 = Node([a,b])
+    print n3
+    print type(n3)
+    inputs = [[0,0],[0,1],[1,1],[1,0]]
+    outputs = [np.array([0]),np.array([1]),np.array([0]),np.array([1])]
+    net = ITM()
+    for ins,out in zip(inputs, outputs):
+        net.update(ins, out)
+        
+    print net.test([0.5,0.6])
+    

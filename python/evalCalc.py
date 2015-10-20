df27e0a1dbde05cafb4769ffd22b440eb5a8dde4
Jan Pöppel <jpoeppel@techfak.uni-bielefeld.de>
2015-10-20 23:58:49 +0200

    Update evalCalc.py
    Started extraction of relevant evaluation data

--- python/evalCalc.py
+++ python/evalCalc.py
@@ -5,10 +5,51 @@ Created on Fri Sep  4 19:15:35 2015
 
 @author: jpoeppel
 """
-
+#
 import numpy as np
+#
+#
+#data = np.loadtxt("../data/gateModel10Runs_Gate_Act_NoDynsITMNewWinner.txt", delimiter=";")
+#
+#print np.mean(data,axis = 0)
+
+#data = np.loadtxt("./testData.txt", delimiter=";")
+#print data
+"""
+New records
+"""
 
 
-data = np.loadtxt("../data/gateModel10Runs_Gate_Act_NoDynsITMNewWinner.txt", delimiter=";")
+import os
 
-print np.mean(data,axis = 0)
+#directory = "../evalData/"
+directory = "./"
+
+fileList = os.listdir(directory)
+
+for f in fileList:
+    if "config" in f:
+        break
+    
+    if "evalTest" in f:
+        #load data
+        print "filename: ", directory+f
+        data = np.loadtxt(directory+f, delimiter = ';')
+        
+        trainrows = data[:,1] == 1
+        
+        testrows = np.invert(trainrows)
+        testData = data[testrows,:]
+        testDifs = np.copy(testData[:,:14])
+        testDifs[:,5:14] -= testData[:,14:]
+        
+        lastFrames = testData[:,3] ==0
+        lastFrames = np.roll(lastFrames, -1) #TODO Test if this is always correct!
+        
+        #Consider filtering, i.e. only last frame
+        actDifs = testDifs[:,12:14]
+        oriDifs = testDifs[:,11]
+        posDifs = testDifs[:,5:7]
+        keyPoint1Difs = testDifs[:,7:9]
+        keyPoint2Difs = testDifs[:,9:11]
+        

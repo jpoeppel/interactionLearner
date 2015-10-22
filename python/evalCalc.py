cfedf2a787b1992de4b0f65e66709ba3fbcdbbb1
BlackZ <jpoeppel@techfak.uni-bielefeld.de>
2015-10-22 23:03:11 +0200

    added logic to plot testpos specific data

--- python/evalCalc.py
+++ python/evalCalc.py
@@ -38,8 +38,8 @@ ysOri = []
 errorsPos = []
 errorsOri = []
 
-
-
+trainRunOrder = []
+testPosValues = {}
 
 for f in sorted(fileList, key=natSort):
     
@@ -47,11 +47,12 @@ for f in sorted(fileList, key=natSort):
         continue
     if "Configuration_44" in f:
         continue
-    if "gateModel" in f:
-        continue
+#    if "gateModel" in f:
+#        continue
     nameOnly = f.replace('.txt','')
     parts = nameOnly.split("_")
     numTrainRuns = int(parts[parts.index("TrainRuns")-1])
+    trainRunOrder.append(numTrainRuns)
     currentConfig = int(parts[parts.index("Configuration")+1])
     xs.append(numTrainRuns)
     #load data
@@ -65,8 +66,11 @@ for f in sorted(fileList, key=natSort):
     testDifs = np.copy(testData[:,:14])
     testDifs[:,5:14] -= testData[:,15:]
     
-    lastFrames = testData[:,3] ==0
-    lastFrames = np.roll(lastFrames, -1) #TODO Test if this is always correct!
+    firstFrames = testData[:,3] == 0
+    
+    lastFrames = np.roll(firstFrames, -1) #TODO Test if this is always correct!    
+
+    startingPositionsX = testData[firstFrames,12]    
     
     #Consider filtering, i.e. only last frame
     actDifs = testDifs[lastFrames,12:14]
@@ -74,6 +78,25 @@ for f in sorted(fileList, key=natSort):
     posDifs = testDifs[lastFrames,5:7]
     keyPoint1Difs = testDifs[lastFrames,7:9]
     keyPoint2Difs = testDifs[lastFrames,9:11]
+    
+    numTestRuns = int(np.max(testData[:,2]))+1
+    print numTestRuns
+    testPosRes = {}
+    startingTestPos = np.zeros(numTestRuns)
+    #Consider each testpos separetly
+    for i in xrange(numTestRuns):
+        iMask = testData[lastFrames,2] == i
+        posDifsTmp = posDifs[iMask]
+        oriDifsTmp = oriDifs[iMask]
+        testPosRes[i] = (np.mean(np.linalg.norm(posDifsTmp, axis=1)), np.mean(oriDifsTmp), 
+                        np.std(np.linalg.norm(posDifsTmp, axis=1)), np.std(oriDifsTmp))
+        startPosMask = testData[:,2] == i
+        startingTestPos[i] = np.mean(testData[firstFrames*startPosMask,12])
+#        print startingTestPos
+    testPosValues[numTrainRuns] = testPosRes
+        
+    
+    
     
     ysPos.append(np.mean(np.linalg.norm(posDifs, axis=1)))
     ysOri.append(np.mean(oriDifs))
@@ -99,5 +122,26 @@ ax1.set_xlim([0,35])
 
 #pp.savefig()
 #pp.close()
+
+numSubPlotRows = len(testPosValues)
+fig, axes = plt.subplots(numSubPlotRows, 2)
+
+i = 0
+for row in axes:
+    resDict = testPosValues[trainRunOrder[i]]
+    xs = startingTestPos
+    print "xs: ", xs
+    ysPos = [resDict[j][0] for j in xrange(len(resDict))]
+    print "trainRuns: ", trainRunOrder[i]
+    print "yPos: ", ysPos
+    errorsPos = [resDict[j][2] for j in xrange(len(resDict))]  
+    ysOri = [resDict[j][1] for j in xrange(len(resDict))]
+    errorsOri = [resDict[j][3] for j in xrange(len(resDict))]
+    row[0].errorbar(xs, ysPos, yerr = errorsPos, fmt='o')
+    row[1].errorbar(xs, ysOri, yerr = errorsOri, fmt='o')
+#    row[0].set_xlim([-0.4,0.4])
+#    row[1].set_xlim([-0.4,0.4])
+    i+=1
+
 
 plt.show()

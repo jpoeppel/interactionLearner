2687637126113f80d4b978111768323721ce538e
Jan Pöppel <jpoeppel@techfak.uni-bielefeld.de>
2015-10-22 16:45:10 +0200

    added first plot calculation

--- python/evalCalc.py
+++ python/evalCalc.py
@@ -22,16 +22,38 @@ New records
 
 import os
 
+import re
+
+def natSort(s, _nsr=re.compile('([0-9]+)')):
+    return [int(text) if text.isdigit() else text.lower() for text in re.split(_nsr, s)]
+
 directory = "../evalData/"
 #directory = "./"
 
-fileList = os.listdir(directory)
+fileList = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory,f))]
+
+xs = []
+ysPos = []
+ysOri = []
+errorsPos = []
+errorsOri = []
 
 
-for f in fileList:
-    if f.startswith(".") or "_config" in f:
-        continue
+
+
+for f in sorted(fileList, key=natSort):
     
+    if f.startswith(".") or "_config" in f or "ITM" in f:
+        continue
+    if "Configuration_44" in f:
+        continue
+    if "gateModel" in f:
+        continue
+    nameOnly = f.replace('.txt','')
+    parts = nameOnly.split("_")
+    numTrainRuns = int(parts[parts.index("TrainRuns")-1])
+    currentConfig = int(parts[parts.index("Configuration")+1])
+    xs.append(numTrainRuns)
     #load data
     print "filename: ", directory+f
     data = np.loadtxt(directory+f, delimiter = ';')
@@ -53,5 +75,29 @@ for f in fileList:
     keyPoint1Difs = testDifs[lastFrames,7:9]
     keyPoint2Difs = testDifs[lastFrames,9:11]
     
-    print "oriDifs: ", oriDifs
-        
+    ysPos.append(np.mean(np.linalg.norm(posDifs, axis=1)))
+    ysOri.append(np.mean(oriDifs))
+    errorsPos.append(np.std(np.linalg.norm(posDifs, axis=1)))
+    errorsOri.append(np.std(oriDifs))
+    
+    
+    
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+pp = PdfPages("../gateEvaConfig12.pdf")
+
+
+fig, (ax0, ax1) = plt.subplots(nrows=2, sharex=True, figsize=(15,6))
+ax0.axhline(y=0, color ='lightgrey')
+ax0.errorbar(xs,ysOri, yerr=errorsOri, fmt='o')
+ax0.set_title('Mean orientational differences at the end of a run and their standard deviation.')
+ax1.axhline(y=0, color ='lightgrey')
+ax1.errorbar(xs,ysPos, yerr=errorsPos, fmt='o')
+ax1.set_title('Mean positional differences at the end of a run and their standard deviation.')
+ax1.set_xlim([0,35])
+
+#pp.savefig()
+#pp.close()
+
+plt.show()
